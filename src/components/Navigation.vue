3d8f2b82539a1b099e6d244b2403422af9ba002c
17847 <1286647929@qq.com>
2023-08-25 18:15:31 +0800

    测试模版

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { getRoutes } from '@/plugins/router'
-// import { SwitchIcon } from 'vue-dark-switch'
+import { SwitchIcon } from 'vue-dark-switch'
 
 const { t } = useI18n()
 
@@ -21,10 +21,11 @@ const routes = getRoutes()
 
 		return { path, name: name.toString().slice(1).replaceAll('/', ' · ') }
 	})
-const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
 	console.log(key, keyPath)
 }
+
+const active = ref('0')
 </script>
 
 <template>
@@ -33,28 +34,69 @@ const handleSelect = (key: string, keyPath: string[]) => {
 	<!--					{{ t(r.name) }}-->
 	<!--				</RouterLink>-->
 	<!--			</li>-->
-	<el-menu
-		:default-active="activeIndex"
-		mode="horizontal"
-		:ellipsis="false"
-		:router="true"
-		@select="handleSelect"
-	>
-		<el-menu-item
-			v-for="r of routes"
-			:key="r.path"
-			:index="r.path"
-			:route="r.path"
+	<div style="margin-top: 20px; flex: 1">
+		<el-menu
+			:default-active="active"
+			mode="horizontal"
+			:ellipsis="false"
+			:router="true"
+			@select="handleSelect"
 		>
-			{{ t(r.name) }}
-		</el-menu-item>
-		<div class="flex-grow" />
-		<el-menu-item>hao</el-menu-item>
-	</el-menu>
+			<span class="h-10 w-10 flex items-center justify-center">
+				<SwitchIcon unmount-persets />
+			</span>
+			<div>
+				<el-image
+					style="margin-left: 80px; margin-bottom: 12px; margin-right: 10px"
+					src="https://res.tuwan.com/templet/play/index/images/yuewanlogo3.png"
+				></el-image>
+			</div>
+			<el-menu-item
+				v-for="(r, index) of routes"
+				:key="r.path"
+				:index="index.toString()"
+				:route="r.path"
+				class="menu_header"
+			>
+				{{ t(r.name) }}
+			</el-menu-item>
+			<div class="flex-grow" />
+			<el-menu-item>{{ t('Registration/Login') }}</el-menu-item>
+			<div class="drop">
+				<Dropdown />
+			</div>
+		</el-menu>
+	</div>
 </template>
 
-<style scoped="scoped" lang="scss">
+<style scoped lang="scss">
 .flex-grow {
 	flex-grow: 1;
 }
+
+.drop {
+	//margin-top: 10px;
+	text-align: center;
+	vertical-align: center;
+}
+
+.menu_header {
+	margin-left: 15px;
+}
+//
+//.el-carousel__item h3 {
+//	color: #475669;
+//	opacity: 0.75;
+//	line-height: 300px;
+//	margin: 0;
+//	text-align: center;
+//}
+//
+//.el-carousel__item:nth-child(2n) {
+//	background-color: #99a9bf;
+//}
+//
+//.el-carousel__item:nth-child(2n + 1) {
+//	background-color: #d3dce6;
+//}
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,40 +1,39 @@
 <template>
-	<div>
-		<div class="grid grid-cols-3">
-			<a href="https://github.com/dishait/tov-template" target="_blank">
-				<img src="/logo.png" class="logo" alt="Vite logo" />
-			</a>
-			<a href="https://vitejs.dev" target="_blank">
-				<img src="/vite.svg" class="logo" alt="Vite logo" />
-			</a>
-			<a href="https://vuejs.org/" target="_blank">
-				<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-			</a>
+	<div
+		style="
+			width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		"
+	>
+		<div style="width: 50%">
+			<el-carousel :interval="5000" arrow="always">
+				<el-carousel-item v-for="item in 4" :key="item">
+					<h3 text="2xl">{{ item }}</h3>
+				</el-carousel-item>
+			</el-carousel>
 		</div>
-		<HelloWorld msg="Tov + Vite + Vue" />
 	</div>
 </template>
 
-<style>
-a {
-	color: rgba(37, 99, 235);
+<script setup>
+// const { t } = useI18n()
+</script>
+<style scoped lang="scss">
+.el-carousel__item h3 {
+	color: #475669;
+	opacity: 0.75;
+	line-height: 300px;
+	margin: 0;
+	text-align: center;
 }
 
-p {
-	padding: 0 10px;
+.el-carousel__item:nth-child(2n) {
+	background-color: #99a9bf;
 }
 
-.logo {
-	width: 10em;
-	height: 10em;
-	padding: 1.5rem;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
+.el-carousel__item:nth-child(2n + 1) {
+	background-color: #d3dce6;
 }
 </style>
